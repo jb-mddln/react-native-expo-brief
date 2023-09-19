e01eaa7eac13be2598c3022a983b902814fc428e
jb-mddln <13810686+jb-mddln@users.noreply.github.com>
2023-09-19 13:43:04 +0200

    ui: working on the basic app layout

--- App.js
+++ App.js
@@ -1,12 +1,41 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TextInput, Pressable, View } from 'react-native';
+
+import { SafeAreaProvider, useSafeAreaInsets } from 'react-native-safe-area-context';
+import Button from './components/Button';
+
+const Header = () => {
+  return (
+    <View style={styles.header}>
+      <Text style={styles.headerText}>SimplonVille - Alerte</Text>
+    </View>
+  );
+};
+
+const Form = () => {
+  const insets = useSafeAreaInsets();
+  const buttonProps = {
+    title: 'J\'Alerte ma Ville',
+    icon: 'bell-o',
+    onPress: () => {
+      console.log('click')
+    }
+  };
+  return (
+    <View style={[ styles.container, { paddingTop: insets.top } ]}>
+      <View style={styles.bottom}>
+        <Button props={buttonProps} />
+      </View>
+    </View>
+  );
+}
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaProvider>
+      <Header />
+      <Form />
+    </SafeAreaProvider>
   );
 }
 
@@ -17,4 +46,18 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  bottom: {
+    flex: 1,
+    justifyContent: 'flex-end',
+    marginBottom: 30
+  },
+  header: {
+    backgroundColor: '#42a5f5',
+    padding: 10,
+  },
+  headerText: {
+    color: 'white',
+    fontSize: 18,
+    fontWeight: 'bold',
+  }
 });
