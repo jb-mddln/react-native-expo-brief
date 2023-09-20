97749ac6ae9d2cd8d8c97a86973d3701f269ba9a
jb-mddln <13810686+jb-mddln@users.noreply.github.com>
2023-09-20 14:48:14 +0200

    ui added MapViewer

--- App.js
+++ App.js
@@ -1,9 +1,20 @@
-import { StyleSheet, Text, TextInput, View, Image, ScrollView } from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+  Image,
+  ScrollView,
+  KeyboardAvoidingView,
+  Platform
+} from 'react-native';
 import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
-import FontAwesome from "@expo/vector-icons/FontAwesome";
-import SelectDropdown from 'react-native-select-dropdown'
+import FontAwesome from '@expo/vector-icons/FontAwesome';
+import SelectDropdown from 'react-native-select-dropdown';
+import MapView, { Marker } from 'react-native-maps';
 import Button from './components/Button';
-
+import Input from './components/Input';
+import { useState } from 'react';
 
 const Header = () => {
   return (
@@ -14,72 +25,138 @@ const Header = () => {
 };
 
 const Form = () => {
-  const alertTypes = ['Voirie', 'Stationnement', 'Travaux', 'Divers']
+  const [lastName, setLastName] = useState('');
 
-  const buttonProps = {
-    title: 'J\'Alerte ma Ville',
-    icon: 'bell-o',
-    onPress: () => {
-      console.log('click')
-    }
-  };
+  const[mapViewerRegion, setMapViewerRegion] = useState({
+    latitude: 50.633333,
+    longitude: 3.066667,
+    latitudeDelta: 0.015,
+    longitudeDelta: 0.0121,
+  });
+
+  const alertTypes = ['Voirie', 'Stationnement', 'Travaux', 'Divers'];
+
+  const handleSubmit = () => {
+    console.log('click');
+    console.log(lastName);
+  }
+
   return (
     <View style={styles.container}>
-      <Text style={[styles.p, {}]}>Merci de sélectionner le type d'alerte dans la liste : </Text>
-      <ScrollView
-          showsVerticalScrollIndicator={false}
-          alwaysBounceVertical={false}
-          contentContainerStyle={styles.scrollViewContainer}>
-        <SelectDropdown
-          data={alertTypes}
-          defaultValueByIndex={0}
-          onSelect={(selectedItem, index) => {
-            console.log(selectedItem, index)
-          }}
-          buttonTextAfterSelection={(selectedItem, index) => {
-            // text represented after item is selected
-            // if data array is an array of objects then return selectedItem.property to render after item is selected
-            return selectedItem
-          }}
-          rowTextForSelection={(item, index) => {
-            // text represented for each item in dropdown
-            // if data array is an array of objects then return item.property to represent item in dropdown
-            return item
-          }}
-          buttonStyle={styles.dropdown1BtnStyle}
-          buttonTextStyle={styles.dropdown1BtnTxtStyle}
-          renderDropdownIcon={isOpened => {
-            return <FontAwesome name={isOpened ? 'chevron-up' : 'chevron-down'} color={'#444'} size={18} />;
-          }}
-          dropdownIconPosition={'right'}
-          dropdownStyle={styles.dropdown1DropdownStyle}
-          rowStyle={styles.dropdown1RowStyle}
-          rowTextStyle={styles.dropdown1RowTxtStyle}
+      <Text style={styles.p}>
+        Merci de bien vouloir entrer vos informations :
+      </Text>
+      <View
+        style={{
+          flexDirection: 'row',
+          justifyContent: 'space-between'
+        }}
+      >
+        <Input placeholder='Entrez votre nom' required width={185} onChangeText={(value) => {
+          console.log(`onChangeText: ${value}`);
+          setLastName(value);
+        }}
         />
-      </ScrollView>
+        <View style={{ marginRight: 10 }} />
+        <Input placeholder='Entrez votre prénom' required  width={185}/>
+      </View>
+      <View>
+        <Input placeholder='Entrez votre email' required width={380} />
+      </View>
+      <Text style={styles.p}>
+        Merci de sélectionner le type d'alerte que vous souhaitez signaler dans la liste :{' '}
+      </Text>
+      <SelectDropdown
+        data={alertTypes}
+        defaultValueByIndex={0}
+        onSelect={(selectedItem, index) => {
+          console.log(selectedItem, index);
+        }}
+        buttonTextAfterSelection={(selectedItem, index) => {
+          return selectedItem;
+        }}
+        rowTextForSelection={(item, index) => {
+          return item;
+        }}
+        buttonStyle={styles.dropdown1BtnStyle}
+        buttonTextStyle={styles.dropdown1BtnTxtStyle}
+        renderDropdownIcon={isOpened => {
+          return (
+            <FontAwesome
+              name={isOpened ? 'chevron-up' : 'chevron-down'}
+              color={'#444'}
+              size={18}
+            />
+          );
+        }}
+        dropdownIconPosition={'right'}
+        dropdownStyle={styles.dropdown1DropdownStyle}
+        rowStyle={styles.dropdown1RowStyle}
+        rowTextStyle={styles.dropdown1RowTxtStyle}
+      />
+      <View style={{ margin: 10 }}>
+        <Input 
+          placeholder='Décrivez brièvement le problème que vous souhaitez signaler.' 
+          required 
+          width={380} 
+          height={180}
+          multiline 
+          numberOfLines={8} 
+        />
+        <MapView
+          style={{width: 380, height: 280}}
+          region={mapViewerRegion}
+        >
+        <Marker 
+          draggable 
+          coordinate={mapViewerRegion} 
+          title='Emplacement à signaler'
+          onDragEnd={(e) => setMapViewerRegion(e.nativeEvent.coordinate)}
+        />
+        </MapView>
+      </View>
       <View style={styles.bottom}>
-        <Button props={buttonProps} />
+        <Button props={{title: 'J\'Alerte ma Ville', icon:'bell-o'}} onPress={handleSubmit} />
       </View>
     </View>
   );
-}
+};
 
 export default function App() {
   return (
     <SafeAreaProvider>
-      <SafeAreaView style={{ backgroundColor: '#42a5f5' }} >
+      <SafeAreaView style={{ backgroundColor: '#42a5f5' }}>
         <Header />
       </SafeAreaView>
-      <View style={{ width: '100%', height: 250 }}>
-        <Image
-          style={{ width: '100%', height: '100%', objectPosition: 'center top' }}
-          source={require("./assets/placeholder.jpg")}
-        />
-      </View>
-      <Text style={styles.p}>
-        Ceci est un formulaire de signalement vous permettant d'alerter directement les services municipaux lorsque vous rencontrez un dysfonctionnement dans votre ville.(Ex. : propreté, voirie, éclairage public...)
-      </Text>
-      <Form />
+      <KeyboardAvoidingView behavior={Platform.OS === 'ios' ? 'padding' : null}>
+        <ScrollView
+          showsVerticalScrollIndicator={false}
+          alwaysBounceVertical={false}
+          contentContainerStyle={styles.scrollViewContainer}
+        >
+          <View style={{ width: '100%', height: 250 }}>
+            <Image
+              style={{
+                width: '100%',
+                height: '100%',
+                objectPosition: 'center top'
+              }}
+              source={require('./assets/placeholder.jpg')}
+            />
+          </View>
+          <Text style={styles.p}>
+            Ceci est un formulaire de signalement vous permettant d'alerter
+            directement les services municipaux lorsque vous rencontrez un
+            dysfonctionnement dans votre ville.(Ex. : propreté, voirie,
+            éclairage public...)
+          </Text>
+          <Text style={styles.p}>
+            Les champs du formulaire marqués par un * sont obligatoires et
+            doivent être correctement remplis.
+          </Text>
+          <Form />
+        </ScrollView>
+      </KeyboardAvoidingView>
     </SafeAreaProvider>
   );
 }
@@ -89,33 +166,33 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'center'
   },
   bottom: {
     flex: 1,
     justifyContent: 'flex-end',
+    padding: 10,
     marginBottom: 30
   },
   header: {
     backgroundColor: '#42a5f5',
-    padding: 10,
+    padding: 10
   },
   headerText: {
     color: 'white',
     fontSize: 18,
-    fontWeight: 'bold',
+    fontWeight: 'bold'
   },
   p: {
     textAlign: 'justify',
     padding: 10
   },
-
   scrollViewContainer: {
     flexGrow: 1,
     justifyContent: 'space-between',
     alignItems: 'center',
-    paddingVertical: '10%',
-    paddingBottom: '20%',
+    // paddingVertical: '10%',
+    paddingBottom: '20%'
   },
   dropdown1BtnStyle: {
     width: 250,
@@ -123,10 +200,13 @@ const styles = StyleSheet.create({
     backgroundColor: '#FFF',
     borderRadius: 8,
     borderWidth: 1,
-    borderColor: '#444',
+    borderColor: '#444'
   },
-  dropdown1BtnTxtStyle: {color: '#444', textAlign: 'left'},
-  dropdown1DropdownStyle: {backgroundColor: '#EFEFEF'},
-  dropdown1RowStyle: {backgroundColor: '#EFEFEF', borderBottomColor: '#C5C5C5'},
-  dropdown1RowTxtStyle: {color: '#444', textAlign: 'left'},
+  dropdown1BtnTxtStyle: { color: '#444', textAlign: 'left' },
+  dropdown1DropdownStyle: { backgroundColor: '#EFEFEF' },
+  dropdown1RowStyle: {
+    backgroundColor: '#EFEFEF',
+    borderBottomColor: '#C5C5C5'
+  },
+  dropdown1RowTxtStyle: { color: '#444', textAlign: 'left' },
 });
---
+++ components/Input.js
@@ -0,0 +1,42 @@
+import React, { useState } from 'react';
+import { TextInput, StyleSheet } from 'react-native';
+
+export default function Input({ placeholder = '', required = false, width = '100%', height = 50, editable = true, multiline = false, numberOfLines = 0, onChangeText }) {
+  const [text, setText] = useState('');
+  const [isFocused, setIsFocused] = useState(false);
+
+  const borderStyle =
+    required && isFocused && text === ''
+      ? { borderColor: 'red', outline: '1px solid rgb(255, 0, 0)' }
+      : {};
+
+  return (
+    <TextInput
+      placeholder={required ? `${placeholder} *` : placeholder}
+      style={[styles.input, borderStyle, { width: width }, { height: height }]}
+      onChangeText={value => {
+        setText(value);
+        onChangeText(value);
+      }}
+      onFocus={() => setIsFocused(true)}
+      onBlur={() => setIsFocused(false)}
+      editable={editable}
+      multiline={multiline}
+      numberOfLines={numberOfLines}
+    />
+  );
+}
+
+const styles = StyleSheet.create({
+  input: {
+    color: '#444',
+    textAlign: 'left',
+    height: 50,
+    backgroundColor: '#FFF',
+    borderRadius: 8,
+    borderWidth: 1,
+    borderColor: '#444',
+    paddingHorizontal: 10,
+    marginBottom: 10
+  }
+});
